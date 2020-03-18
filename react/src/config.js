adb8d440030d9f8cc0bd63b8b3f7bd8c566adf39
dbrowski <anthonydombrowski@pingidentity.com>
2020-03-18 10:14:51 -0500

    Configure custom logo

--- react/src/config.js
+++ react/src/config.js
@@ -1,22 +1,22 @@
 const common = {
   branding: {
-    logo: require('./logo.png')
+    logo: require("./pingoneforcustomerslogo.png")
   }
 };
 
 const stg = {
-  AUTH: 'https://auth-staging.pingone.com',
-  API: 'https://api-staging.pingone.com/v1/environments'
+  AUTH: "https://auth-staging.pingone.com",
+  API: "https://api-staging.pingone.com/v1/environments"
 };
 
 const prod = {
-  AUTH: 'https://auth.pingone.com',
-  API: 'https://api.pingone.com/v1/environments'
+  AUTH: "https://auth.pingone.com",
+  API: "https://api.pingone.com/v1/environments"
 };
 
 // Default to prod if not set
 let config = prod;
-if (process.env.REACT_APP_STAGE === 'stg') {
+if (process.env.REACT_APP_STAGE === "stg") {
   config = stg;
 }
 
